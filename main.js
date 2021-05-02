e66cb556e5436226305e9b15aad4a743ed4eb2f5
Chris <chris.cebrero@gmail.com>
2021-05-02 12:41:46 -0700

    added phonetic mappings

---
+++ main.js
@@ -0,0 +1,15 @@
+// Initialize the map for the function, with each number corresponding to its phonetic counterpart
+const map = {
+    '1' : 'One',
+    '2' : 'Two',
+    '3' : 'Three',
+    '4' : 'Four',
+    '5' : 'Five',
+    '6' : 'Six',
+    '7' : 'Seven',
+    '8' : 'Eight',
+    '9' : 'Nine',
+    '0' : 'Zero'
+}
+// I used the string '1' instead of the integer 1 because on terminal when passing extra parameters,
+// i.e. (node main.js 3) the 3 is processed as a string which makes it easier to write the funcion by abit
